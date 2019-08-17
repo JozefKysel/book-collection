5ca4fbec4a5d9dd00f9f22320b5f795ce0ba6ee4
JozefKysel <jojo.kysel00@gmail.com>
2019-08-17 11:14:21 +0200

    Feat typescript and express initial setup

---
+++ server/src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+const app = express();
+const port = 8080;
+
+app.get( "/", ( req: any, res: any ) => {
+    res.send( "Hello world!" );
+} );
+
+app.listen( port, () => {
+    // tslint:disable-next-line:no-console
+    console.log(`Server is listening on port: ${ port }`);
+});
